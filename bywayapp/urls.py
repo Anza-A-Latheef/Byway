ded8b39275a684e3b00a33e78beb343cbfe49d36
Anza-A-Latheef <an7anz@gmail.com>
2024-07-20 17:20:15 +0530

    modified:   Templates/home.html 	new file:   Templates/singlepage.html 	modified:   bywayapp/__pycache__/urls.cpython-312.pyc 	modified:   bywayapp/__pycache__/views.cpython-312.pyc 	modified:   bywayapp/urls.py 	modified:   bywayapp/views.py 	new file:   static/css/singlepage.css 	renamed:    static/images/span-6.svg -> static/images/Review.svg 	new file:   static/images/creator.png 	new file:   static/images/medal.svg 	new file:   static/images/play-button.svg 	new file:   static/images/social2.png

--- bywayapp/urls.py
+++ bywayapp/urls.py
@@ -4,5 +4,6 @@ from.import views
 urlpatterns = [
     path('',views.home,name='home'),
     # path('clickbutton/',views.clickbutton,name='clickbutton'),
+    path('singlepage/', views.pageRoute, name='singlepage'),
 
 ]
--- bywayapp/views.py
+++ bywayapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request,"home.html")
 
-# def clickbutton(request):
-#     return render(request, "hh.html")
+def pageRoute(request):
+    return render(request, "singlepage.html")
