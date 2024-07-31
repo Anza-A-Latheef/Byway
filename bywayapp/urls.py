010bb3929e8ac612fb448ec552c6262b9edf0d37
Anza-A-Latheef <an7anz@gmail.com>
2024-07-31 10:24:06 +0530

    modified:   Templates/coursespage.html 	modified:   Templates/home.html 	modified:   bywayapp/__pycache__/urls.cpython-312.pyc 	modified:   bywayapp/urls.py 	modified:   static/css/courses.css 	modified:   static/css/singlepage.css

--- bywayapp/urls.py
+++ bywayapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('singlepage/<int:pk>', views.pageRoute, name='singlepage'),
     path('coursespage/', views.listCourses, name='coursespage'),
+    path('listCourses/',views.listCourses, name='listCourses'),
     path('ViewCategory/<int:pk>',views.ViewCategory,name='ViewCategory'),
 ]
